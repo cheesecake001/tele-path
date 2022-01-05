61db7b2362680efd9d70942fcb979212781c6d44
cheesecake001 <rupalisingh7502@gmail.com>
2022-01-05 16:26:22 +0530

    telepath

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { ChatEngine } from "react-chat-engine";
 import './App.css';
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
-function App() {
+const projectID="df653679-8234-420c-a5e3-a1a895a1aff1"
+
+const App = () => {
+
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={ (ChatProps) => <ChatFeed {...ChatProps} />}
+        onNewMessage={ () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play() }
+      />
   );
 }
 
